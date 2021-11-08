8f39d81c62a23100179f5bf85f55575d1d1da577
ncarette <thefamousnono@gmail.com>
2021-11-08 16:57:01 +0100

    first working code

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+
 function lancer100faces(){
     let lancerdé = Math.floor((Math.random() * 100) + 1);
     console.log(lancerdé);
-    résultat.innerHTML += lancerdé + " | "
+    result.innerHTML += lancerdé + " | "
 }
 
 function randomize(){
-  résultat.innerHTML = "Pompi"
+
+	let hb1 = ["Bon","Happy", "Bonne", "H.", "Joyeux","alles Gute zum"];
+	let hb2 = [" anniversaire", " anni", " Geburtstag", " birthday", " B.", " annif"," B-day", " днем рождения"];
+	let nom = [" Ponpi", " Pénélope", " Péné", " Loop", " Pomp", " la meuf à Ulysse", " Пенелопа", " Pompi"];
+	let photos = ["empty.jpg"];
+	let font = ["Aldrich","Arial"]
+
+    let fullWidth = window.innerWidth;
+    let fullHeight = window.innerHeight;
+
+    let text = hb1[1] + hb2[1] + nom[1];
+
+    let elem = document.createElement("div");
+    elem.textContent = text;
+    elem.style.position = "absolute";
+    elem.style.left = (Math.round(Math.random() * fullWidth)-30) + "px";
+    elem.style.top = (Math.round(Math.random() * fullHeight)-30) + "px";
+    elem.style.fontSize = (Math.round(Math.random() * 30) + 10) + "pt";
+    elem.style.fontFamily = font[1]
+
+    document.body.appendChild(elem);
+
 }
 
-document.querySelector("[id='button']")
-    .addEventListener("click",randomize);
+document.addEventListener("click",randomize);
