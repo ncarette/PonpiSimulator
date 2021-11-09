a0ff5dffefda129c6781dbae97b2ec7c046bfb74
ncarette <thefamousnono@gmail.com>
2021-11-09 11:09:19 +0100

    code working and pretty without images

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-
-function lancer100faces(){
-    let lancerdé = Math.floor((Math.random() * 100) + 1);
-    console.log(lancerdé);
-    result.innerHTML += lancerdé + " | "
-}
+/* A code that able randomizing birthday wishes for Pénélope - no use of d3 nor any external library*/
 
 function randomize(){
 
-	let hb1 = ["Bon","Happy", "Bonne", "H.", "Joyeux","alles Gute zum"];
-	let hb2 = [" anniversaire", " anni", " Geburtstag", " birthday", " B.", " annif"," B-day", " днем рождения"];
-	let nom = [" Ponpi", " Pénélope", " Péné", " Loop", " Pomp", " la meuf à Ulysse", " Пенелопа", " Pompi"];
+	let hb1 = ["Bon","Happy", "Bonne", "H.", "Joyeux","alles Gute zum", "C", "Heureux", "Intense", "Bon", "Super"];
+	let hb2 = [" anniversaire", " anni", " Geburtstag", " birthday", " B.", " annif"," B-day", " днем рождения",
+		" anniv"," années", " jour de naissance", " scorpio season"];
+	let nom = [" Ponpi", " Pénélope", " Péné", " Loop", " Pomp", " la meuf à Ulysse", " Пенелопа", " Pompi"," Pompi", " Pénélope",];
 	let photo = ["empty.jpg"];
-	let font = ["Aldrich","Arial", "Georgia", "Trebuchet MS", "Courier New", "Brush Script MT"]
-	let color = ["black", "gold", "pink", "green", "red", "blue","orange", "purple"]
+	let font = ["Aldrich","Arial", "Verdana", "Andalé Mono", "Courier", "Monaco", "Lucida Console","Comic Sans MS",
+		"Bradley Hand", "Brush Script MT", "Luminari","American Typewriter","Didot","Georgia"]
+	let color = ["black", "gold", "pink", "green", "red","orange", "purple","fuchsia","lime","aqua",
+		"navy","teal","maroon","gray","hotpink","lawngreen","indigo","lightpink","mediumpurple",
+		"olivedrab","yellow","orangered","palegreen","paleturquoise","palevioletred","peru","royalblue",
+		"seagreen","tomato","black","black","yellowgreen"]
+	let texttransform = ["uppercase", "lowercase", "lowercase"]
 
     let fullWidth = window.innerWidth;
     let fullHeight = window.innerHeight;
@@ -25,14 +26,15 @@ function randomize(){
     elem.style.left = (Math.round(Math.random() * fullWidth)-50) + "px";
     elem.style.top = (Math.round(Math.random() * fullHeight)-50) + "px";
     elem.style.fontSize = (Math.round(Math.random() * 40) + 20) + "pt";
-    elem.style.fontFamily = font[font.length * Math.random() | 0]
-    elem.style.color = color[color.length * Math.random() | 0]
+    elem.style.fontFamily = font[font.length * Math.random() | 0];
+    elem.style.color = color[color.length * Math.random() | 0];
+    elem.style.textTransform = texttransform[texttransform.length * Math.random() | 0];
+    elem.style.letterSpacing = (Math.round(Math.random()*16)) + "px";
 
     document.body.appendChild(elem);
-
     
 
-	if(Math.floor(Math.random() * 3) == 0){
+	if(Math.floor(Math.random() * 3) == 5){
 		let image = document.createElement("img");
     	image.src = photo[photo.length * Math.random() | 0]
     	image.style.position = "absolute";
